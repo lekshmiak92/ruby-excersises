2ef852439fd99923aadeaa2b83abaf0a3b2bc78e
leks92 <lekshmiak92@gmail.com>
2016-08-06 01:27:54 +0530

    moved conditions for divisibility check to its own functions

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,18 @@
+def divisible_by_3?(number)
+  number%3 == 0
+end
+
+def divisible_by_5?(number)
+  number%5 == 0
+end
+
 (1..25).each do |number|
   result = ""
-  if number%3 == 0
+  if divisible_by_3?(number)
     result = "fizz"
   end
-  if number%5 == 0
-    result = result + "buzz"
+  if divisible_by_5?(number)
+    result += "buzz"
   end
   if result == ""
     result = number
