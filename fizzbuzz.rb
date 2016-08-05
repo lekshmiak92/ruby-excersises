ddc3ebcc15eed49b29340fb35fc1cc9129b2c254
leks92 <lekshmiak92@gmail.com>
2016-08-06 00:36:47 +0530

    insteadof printing at each if condition the result is assigned to a variable and printed at end of execution

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,12 @@
 (1..100).each do |number|
   if number%15 == 0
-    puts "fizzbuzz"
+    result = "fizzbuzz"
   elsif number%3 == 0 
-    puts "fizz"
+    result = "fizz"
   elsif number%5 == 0 
-    puts "buzz"
+    result = "buzz"
   else
-    puts number
+    result = number
   end
+  puts result
 end
